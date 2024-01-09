de3fee3f1a413958d4e1d0ea7c2826f587101c89
David Onifade <davidicfola@gmail.com>
2024-01-09 00:44:42 +0000

    build(deps): update package-lock.json, package.json, app.module.ts and ninjas.controller.ts

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateNinjasDto } from './dto/create-ninjas.dto';
+import { UpdateNinjasDto } from './dto/update-ninjas.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  @Get()
+  getNinjas(@Query('type') type: string) {
+    return [{ type }];
+  }
+
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjasDto) {
+    return {
+      name: createNinjaDto.name,
+    };
+  }
+
+  @Put(':id')
+  updateNinja(
+    @Param('id') id: string,
+    @Body() updateNinjasDto: UpdateNinjasDto,
+  ) {
+    return {
+      id,
+      name: updateNinjasDto,
+    };
+  }
+
+  @Patch(':id')
+  patchNinja() {
+    return {};
+  }
+
+  @Delete(':id')
+  removeNinja() {
+    return {};
+  }
+}
