ee0dfc421a45c90eccf853c952de842d84b9a6d2
David Onifade <davidicfola@gmail.com>
2024-01-09 21:53:50 +0000

    update ninjas.controller.ts

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -29,7 +29,7 @@ export class NinjasController {
   @Post()
   createNinja(@Body() createNinjaDto: CreateNinjasDto) {
     return {
-      name: .name,
+      name: createNinjaDto.name,
     };
   }
 
